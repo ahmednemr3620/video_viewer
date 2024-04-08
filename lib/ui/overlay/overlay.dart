e513fc581a74329819ef2b9921d417050694f6a5
Ahmed <ahmednemr3620@gmail.com>
2024-04-08 05:25:44 +0200

    for tv

--- lib/ui/overlay/overlay.dart
+++ lib/ui/overlay/overlay.dart
@@ -23,39 +23,39 @@ class VideoCoreOverlay extends StatelessWidget {
     return CustomOpacityTransition(
       visible: !controller.isShowingThumbnail,
       child: Stack(children: [
-        if (header != null)
-          CustomSwipeTransition(
-            axisAlignment: 1.0,
-            visible: overlayVisible,
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: GradientBackground(
-                child: header,
-                end: Alignment.topCenter,
-                begin: Alignment.bottomCenter,
-              ),
-            ),
-          ),
-        BottomCenterAlign(
-          child: CustomSwipeTransition(
-            visible: overlayVisible,
-            axisAlignment: -1.0,
-            child: const OverlayBottom(),
-          ),
-        ),
-        AnimatedBuilder(
-          animation: controller,
-          builder: (_, __) => CustomOpacityTransition(
-            visible: overlayVisible,
-            child: const Center(
-              child: PlayAndPause(type: PlayAndPauseType.center),
-            ),
-          ),
-        ),
-        CustomOpacityTransition(
-          visible: controller.isShowingSettingsMenu,
-          child: const SettingsMenu(),
-        ),
+        // if (header != null)
+          // CustomSwipeTransition(
+          //   axisAlignment: 1.0,
+          //   visible: overlayVisible,
+          //   child: Align(
+          //     alignment: Alignment.topLeft,
+          //     child: GradientBackground(
+          //       child: header,
+          //       end: Alignment.topCenter,
+          //       begin: Alignment.bottomCenter,
+          //     ),
+          //   ),
+          // ),
+        // BottomCenterAlign(
+        //   child: CustomSwipeTransition(
+        //     visible: overlayVisible,
+        //     axisAlignment: -1.0,
+        //     child: const OverlayBottom(),
+        //   ),
+        // ),
+        // AnimatedBuilder(
+        //   animation: controller,
+        //   builder: (_, __) => CustomOpacityTransition(
+        //     visible: overlayVisible,
+        //     child: const Center(
+        //       child: PlayAndPause(type: PlayAndPauseType.center),
+        //     ),
+        //   ),
+        // ),
+        // CustomOpacityTransition(
+        //   visible: controller.isShowingSettingsMenu,
+        //   child: const SettingsMenu(),
+        // ),
       ]),
     );
   }
